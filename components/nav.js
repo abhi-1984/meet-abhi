e72a898fd221c6a4c90ca2d37df36ab0d8f8e49d
Abhishek <abhishekkambli3@gmail.com>
2021-01-10 20:57:35 +0530

    added nav interaction

--- components/nav.js
+++ components/nav.js
@@ -1,11 +1,33 @@
 import Link from "next/link";
+import Image from "next/image";
+import React, { useState, useEffect } from "react";
 
 export default function Nav({ handleGetInTouchPopover }) {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollPosition(position);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <nav className="px-10 py-3 fixed top-0 left-0 right-0 bg-black bg-opacity-90 backdrop-blur z-40">
       <div className="wrapper flex items-center justify-between relative">
         <Link href="/">
-          <a className="focus:outline-none text-xl">ab.</a>
+          <a className="focus:outline-none text-xl">
+            {scrollPosition >= 160 ? (
+              <Image src="/assets/common/logo.png" width="32px" height="32px" />
+            ) : (
+              <span>ab.</span>
+            )}
+          </a>
         </Link>
         <div className="grid grid-flow-col auto-cols-max gap-6 sm:gap-10 items-center relative z-10">
           <Link href="/">
