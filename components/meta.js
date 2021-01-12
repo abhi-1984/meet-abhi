a1e36237cf8997fcbb79529aca5297b65e2aaa42
Abhishek <abhishekkambli3@gmail.com>
2021-01-12 22:58:35 +0530

    ...

--- components/meta.js
+++ components/meta.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
+import Head from "next/head";
+import { CMS_NAME, HOME_OG_IMAGE_URL } from "../lib/constants";
 
 export default function Meta() {
   return (
@@ -24,7 +24,7 @@ export default function Meta() {
       <link rel="manifest" href="/favicon/site.webmanifest" />
       <link
         rel="mask-icon"
-        href="/favicon/safari-pinned-tab.svg"
+        href="/favicon/safari-pinned-tab.png"
         color="#000000"
       />
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
@@ -36,7 +36,7 @@ export default function Meta() {
         name="description"
         content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
       />
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+      <meta property="og:image" content={"/favicon/ogimage.png"} />
     </Head>
-  )
+  );
 }
